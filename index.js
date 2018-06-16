f94bbcc74ce5e6978a0926f93ffa179e2c519f64
Van Phan <vanthuyphan@gmail.com>
2018-06-16 12:38:50 -0500

    clean up

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const util = require('util');
 const config = require('./config/config');
 const app = require('./config/express');
 
-const debug = require('debug')('express-mongoose-es6-rest-api:index');
+const debug = require('debug')('EtsyAPI:index');
 
 Promise = require('bluebird');
 
